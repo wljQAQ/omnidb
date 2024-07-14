63de01b09c55f4ac51a736ab9a3f617bbfeef2a7
wljQAQ <wulongjiang1@gmail.com>
2024-07-14 23:39:24 +0800

    '项目初始化'

--- packages/web/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  console.log(11);
-  return <div>App</div>;
-}
---
+++ packages/web/src/app/page.tsx
@@ -0,0 +1,18 @@
+import { Layout } from 'antd';
+
+const { Header, Content, Footer, Sider } = Layout;
+
+export default function Home() {
+  return (
+    <main>
+      <Layout>
+        {/* <Sider>1</Sider>
+        <Layout>
+          <Header>2</Header>
+          <Content>3</Content>
+        </Layout> */}
+        <Content>111</Content>
+      </Layout>
+    </main>
+  );
+}
