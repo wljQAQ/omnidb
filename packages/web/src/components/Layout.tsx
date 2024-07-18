d59a99ed0f7200b5fed4acc672f3e36187f1b09c
wljQAQ <wlj@lilanz.local>
2024-07-18 18:06:33 +0800

    'nestjs——init'

--- packages/web/src/components/Layout.tsx
+++ packages/web/src/components/Layout.tsx
@@ -1,13 +1,21 @@
-import { Layout } from "antd";
+import { Layout ,Button} from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
 
 const { Header, Content, Sider } = Layout;
 
 export default function index() {
   return (
-    <Layout className="h-full">
+    <Layout className="h-full" hasSider>
       <Sider theme="light">
-        <div className="p-3 border-b border-gray-100">
+        <div className="flex items-center border-b border-gray-50 h-16 px-4">
           <img className="w-14" src="../../assets/logo.png" alt="" />
+        </div>
+
+        <div className="py-1 border-gray-50 border-b">
+        <Button className="w-full" type="text"  icon={<PlusOutlined />}>
+          新建项目
+    </Button>
         </div>
       </Sider>
 
