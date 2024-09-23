c72f4143b7e225d6b7de88576beb18b491910166
wljQAQ <wlj@lilanz.local>
2024-09-23 18:00:06 +0800

    'tailwind配置'

--- packages/frontend/component/src/ui/Table/Table.tsx
+++ packages/frontend/component/src/ui/Table/Table.tsx
@@ -106,8 +106,8 @@ export const Table = ({ columns, data: DefaultData, size, globalFilter, columnFi
 
   return (
     <DndContext collisionDetection={closestCenter} modifiers={[restrictToHorizontalAxis]} onDragEnd={handleDragEnd} sensors={sensors}>
-      <div ref={tableContainerRef} className="table-box relative h-full overflow-auto">
-        <table className="relative grid" style={{ transition: 'padding 0.2s ease 0s' }}>
+      <div ref={tableContainerRef} className="relative h-full overflow-auto">
+        <table className="border-divider relative grid border" style={{ transition: 'padding 0.2s ease 0s' }}>
           <Header className="sticky top-0 z-10 grid">
             {table.getHeaderGroups().map(headerGroup => (
               <Row className="flex w-full" key={headerGroup.id}>
---
+++ tailwind.config.cjs
@@ -0,0 +1,44 @@
+import plugin from 'tailwindcss/plugin';
+
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './packages/frontend/component/src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      padding: {
+        small: '0.5rem',
+        middle: '0.75rem',
+        large: '1rem'
+      },
+      textColor: {
+        primary: '--color-primary',
+        secondary: '--color-secondary',
+        muted: '--color-text-muted'
+      },
+      backgroundColor: {
+        containHeader: 'var(--color-base)',
+        primary: '--color-primary',
+        secondary: '--color-secondary',
+        muted: '--color-text-muted'
+      },
+      borderColor: {
+        divider: 'rgb(240, 240, 240)'
+      }
+    }
+  },
+  plugins: [
+    plugin(({ addUtilities }) => {
+      addUtilities({
+        '.flex-center': {
+          display: 'flex',
+          'align-items': 'center',
+          'justify-content': 'center'
+        },
+        '.flex-between': {
+          display: 'flex',
+          'align-items': 'center',
+          'justify-content': 'space-between'
+        }
+      });
+    })
+  ]
+};
