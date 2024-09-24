1b8067603fb09ef017b2a62ed279b08b7d945f2f
wljQAQ <wlj@lilanz.local>
2024-09-24 18:01:43 +0800

    '依赖更新'

--- packages/frontend/component/src/components/bi-menu/bi-menu.tsx
+++ packages/frontend/component/src/components/bi-menu/bi-menu.tsx
@@ -1,3 +1,43 @@
+import React from 'react';
+
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    key: 'sub1',
+    label: 'Navigation One',
+    icon: <MailOutlined />
+  },
+  {
+    key: 'sub2',
+    label: 'Navigation Two',
+    icon: <AppstoreOutlined />
+  },
+  {
+    type: 'divider'
+  },
+  {
+    key: 'sub4',
+    label: 'Navigation Three',
+    icon: <SettingOutlined />
+  },
+  {
+    key: 'grp',
+    label: 'Group',
+    type: 'group'
+  }
+];
+
 export function BIMenu() {
-  return <div>123</div>;
+  const onClick: MenuProps['onClick'] = e => {
+    console.log('click ', e);
+  };
+
+  return (
+    <Menu onClick={onClick} style={{ width: 256 }} defaultSelectedKeys={['1']} defaultOpenKeys={['sub1']} mode="inline" items={items} />
+  );
 }
