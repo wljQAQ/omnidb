0c2168fb00e14cdb0adf974d2f58093f89a843b1
wljQAQ <wlj@lilanz.local>
2024-09-29 17:54:00 +0800

    'graphql集成'

--- packages/backend/server/src/app.module.ts
+++ packages/backend/server/src/app.module.ts
@@ -1,13 +1,30 @@
-import { Module } from '@nestjs/common';
+// import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
-import { UserService } from './user.service';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { PrismaService } from './prisma.service';
+// import { UserService } from './user.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService, UserService, PrismaService]
+// })
+// export class AppModule {}
+
+import { join } from 'path';
+
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DirectiveLocation, GraphQLDirective } from 'graphql';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService, UserService, PrismaService]
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    })
+  ]
 })
 export class AppModule {}
